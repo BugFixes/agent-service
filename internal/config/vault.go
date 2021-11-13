32ea27c84963067bc3533415f7abb31efb3838f6
Keloran <keloran@chewedfeed.com>
2021-11-13 23:07:25 +0000

    fix linting

--- internal/config/vault.go
+++ internal/config/vault.go
@@ -1,22 +1,22 @@
 package config
 
 import (
-  "os"
+	"os"
 
-  bugLog "github.com/bugfixes/go-bugfixes/logs"
-  "github.com/hashicorp/vault/api"
+	bugLog "github.com/bugfixes/go-bugfixes/logs"
+	"github.com/hashicorp/vault/api"
 )
 
 func GetVaultSecrets(vaultAddress, secretPath string) (map[string]interface{}, error) {
-var m = make(map[string]interface{})
+	var m = make(map[string]interface{})
 
 	token := os.Getenv("VAULT_TOKEN")
 	if token == "" {
 		return m, bugLog.Error("token not found")
 	}
 
-  cfg := api.DefaultConfig()
-  cfg.Address = vaultAddress
+	cfg := api.DefaultConfig()
+	cfg.Address = vaultAddress
 	client, err := api.NewClient(cfg)
 	if err != nil {
 		return m, bugLog.Errorf("client: %+v", err)
